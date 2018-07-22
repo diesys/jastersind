aabd79d78ed4b9a7de28367c26aa56acbd959016
sbiego <forsesim@anche.no>
2018-07-22 19:46:00 +0200

    fix fwd/bwd click colours cycling and minors

--- JasterSind.js
+++ JasterSind.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  var colorsList = ["#F23553", "#F7D843", "#69E569", "#406DF6", "#9351D3", "#DB46BE", "#EC8C34", "#36E1EC", "#ECECEC"];//, "#1F1F1F"];
+  var colorsList = ["#f23553", "#f7d843", "#69e569", "#406df6", "#9351d3", "#db46be", "#ec8c34", "#36e1ec", "#ececec"];//, "#1f1f1f"];
+  var colorsListM = ["#F23553", "#F7D843", "#69E569", "#406DF6", "#9351D3", "#DB46BE", "#EC8C34", "#36E1EC", "#ECECEC"];//, "#1F1F1F"];
   var n_colors = 9;
   var configuration = [];
 
@@ -27,20 +28,35 @@ $(document).ready(function () {
   //     }
   //     console.log(act_clr, colorsList.indexOf(rgb2hex(act_clr)));
   // });
-    $('.try.dot').bind("click", function () {
-      var act_clr = $(this).css('background-color'); 
-      var act_ind = colorsList.indexOf(rgb2hex(act_clr));
-      console.log(act_clr, act_ind);
-      
-      if (act_ind == -1) {
+  $('.try.dot').bind("click", function (e) {
+    var act_clrRGB = $(this).css('background-color');
+    var act_clr = rgb2hex(act_clrRGB);
+    var act_ind = Math.max(colorsList.indexOf(act_clr), colorsListM.indexOf(act_clr));
+    if (e.shiftKey) {
+      if (act_ind < 1) {
+        // $('#m1d1').css("background-color", colorsList[0]);
+        $(this).css("background-color", colorsList[n_colors - 1]);
+      } else {
+        // $('#m1d1').css("background-color", colorsList[act_ind + 1]);
+        $(this).css("background-color", colorsList[(act_ind - 1) % (n_colors)]);
+      }
+      console.log('using SHIFT and click, cycling backwards colours');
+    }
+    
+    else {
+      if (act_ind < 0) {
         // $('#m1d1').css("background-color", colorsList[0]);
         $(this).css("background-color", colorsList[0]);
       } else {  
         // $('#m1d1').css("background-color", colorsList[act_ind + 1]);
-        $(this).css("background-color", colorsList[act_ind+1]);
+        $(this).css("background-color", colorsList[( act_ind + 1 ) % (n_colors)]);
       }
-      console.log(act_ind);
+      console.log('using click, cycling forward colours');
+    }
+    console.log('Before click:', act_clr, act_ind);
+    
   });
+
 
   $('.dot.sol').each(function(){
     var clr = colorsList[Math.floor(Math.random() * (n_colors))];
