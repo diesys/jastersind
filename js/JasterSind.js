671d4c00b29fd3b9e4ed313f6cd79bb750874e6f
sbiego <forsesim@anche.no>
2020-04-11 19:10:14 +0200

    new game configuration, better select UI and style

--- js/JasterSind.js
+++ js/JasterSind.js
@@ -7,12 +7,16 @@
 // fare le mosse codificate nell'url, per salvare le partite e per continuarle
 // logo mobile messo meglio dopo la vittoria
 
-var colorsList = ["#f73030", "#f7d843", "#69e569", "#406df6", "#9351d3", "#db46be", "#ec8c34", "#36e1ec", "#f4f4f4"]; //, "#1f1f1f"];
-// var colorsListM = ["#F23553", "#F7D843", "#69E569", "#406DF6", "#9351D3", "#DB46BE", "#EC8C34", "#36E1EC", "#ECECEC"];//, "#1F1F1F"];
+var colorsList = ["#f73030", "#f7d843", "#69e569", "#406df6", "#9351d3", "#db46be", "#ec8c34", "#36e1ec", "#f4f4f4"];
 var answerCol = [colorsList[0], "#ffffff"];
 var n_colors = 9;
-var configuration = [];
-var lastGuess = [-1, -1, -1, -1];
+var game = [];
+    game['solution'] = [],
+    game['colors'] = colorsList,
+    game['lastguess'] = [-1, -1, -1, -1];
+
+var movesC = document.getElementById("movesContainer");
+
 
 var N = 0;
 
@@ -20,16 +24,17 @@ function dotClick (e) {
   // which dot am I?
   dotNumber = parseInt(this.id.split('d')[1]);
   $(this).addClass('active')
+  // game['lastguess'] = game['lastguess']
 
-  colIx = lastGuess[dotNumber];
+  colIx = game['lastguess'][dotNumber];
   // il caso iniziale va trattato, ahimè, a parte
   if (colIx == -1)
     colIx = (e.shiftKey) ? n_colors - 1: 0;
   else
     // tentativo con pulsante destro e centrale non funziona
-    // colIx = lastGuess[dotNumber] + ((e.shiftKey || e.which == 3 || e.which == 3) ? -1 : 1) + n_colors;
-    colIx = lastGuess[dotNumber] + ((e.shiftKey) ? -1 : 1) + n_colors;
-  lastGuess[dotNumber] = colIx % n_colors;
+    // colIx = game['lastguess'][dotNumber] + ((e.shiftKey || e.which == 3 || e.which == 3) ? -1 : 1) + n_colors;
+    colIx = game['lastguess'][dotNumber] + ((e.shiftKey) ? -1 : 1) + n_colors;
+  game['lastguess'][dotNumber] = colIx % n_colors;
   $(this).animate({ backgroundColor: colorsList[colIx % n_colors] }, 200, "swing");
 }
 
@@ -61,39 +66,43 @@ function check (trg, gss) {
 
 
 function submit () {
-  if (lastGuess.indexOf(-1) != -1)
+  // game['lastguess'] = game['lastguess']
+  if (game['lastguess'].indexOf(-1) != -1)
     return;
+  
+  // game['solution'] = game['solution']
 
-  chk = check(configuration, lastGuess);
+  chk = check(game['solution'], game['lastguess']);
   showRes(chk);
+
+  // hai vinto!
   if (chk[0] == 4) {
     // colora la soluzione
     $('.dot.sol').each(function () {
       dotNumber = parseInt(this.id.split('l')[1]);
-      var clr = colorsList[configuration[dotNumber]];
+      var clr = colorsList[game['solution'][dotNumber]];
       $(this).css('background-color', clr);
     });
+
     // mag section
     win_msg = document.createElement("DIV");
     win_msg.classList = "win_message";
     tries = $('.try.dot').length/4;
-    win_msg.innerHTML = "Congrats, you found the combination in " + ((tries == 1) ? '1 try!' : tries + ' tries!');
+    win_msg.innerHTML = "Congrats, you found the combination in " + ((tries == 1) ? '1 try!' : tries + ' tries, using '+ game['colors'].lenght +'colors!');
     document.querySelector('#gameboard').appendChild(win_msg);
 
-    // $("").fadeIn( "slow", function() {});
-    // $("#solutionContainer").animate({ opacity: '1' }, 500, "swing");
+    // mostra nasconde il necessario a fine partita
     $("#solutionContainer").removeClass('hidden');
-    // $("#title").animate({ opacity: '0' }, 500, "swing");
-    // $("#title").css({"transform": "none"}).animate({left: "0px", transform: "none"}, 500, "swing");
     $("#submit").animate({ opacity: '0' }, 200, "swing");
     $("#submit").attr('style', 'display: none');
     $(".win_message").animate({ opacity: '1' }, 800, "swing");
 
-    // hides the menu
-    $('#clr_select').show()
+    // re-enables the color menu
+    $('#clr_select').removeAttr('disabled')
+    $('#clr_select').prop("selectedIndex", 0);
 
   } else {
-    lastGuess = [-1, -1, -1, -1];
+    game['lastguess'] = [-1, -1, -1, -1];
     newTry();
   }
 }
@@ -105,8 +114,6 @@ function newTry() {
     for (i = dots.length - 4; i < dots.length; i++)
       dots[i].onclick = null;
 
-
-  movesC = document.getElementById("movesContainer");
   dotsC = document.createElement("OL");
 
   dotsC.id = "move" + N;
@@ -138,9 +145,10 @@ function newTry() {
   }
   dotsC.appendChild(ansC);
   // if (N) dotsC.appendChild(document.getElementById("submit"));
-  if (N>=0) {
+  if (N >= 0) {
     // dotsC = document.getElementById("move" + (N - 1));
-
+    // da fare meglio? ora cancella il submit e ne ricrea uno...  
+    $('#submit').remove();
     btn = document.createElement("A");
     btn.id = "submit";
     btn.onclick = submit;
@@ -152,7 +160,7 @@ function newTry() {
     btn.appendChild(btnchk);
     dotsC.appendChild(btn);
   }
-  
+
   N++;
 }
 
@@ -180,15 +188,15 @@ function dotClick2(e) {
   // dotNumber = parseInt(this.id.split('d')[1]);
   $(this).addClass('active')
 
-  // colIx = lastGuess[dotNumber];
+  // colIx = game['lastguess'][dotNumber];
   // il caso iniziale va trattato, ahimè, a parte
   // if (colIx == -1)
   //   colIx = (e.shiftKey) ? n_colors - 1 : 0;
   // else
   // tentativo con pulsante destro e centrale non funziona
-  // colIx = lastGuess[dotNumber] + ((e.shiftKey || e.which == 3 || e.which == 3) ? -1 : 1) + n_colors;
-  // colIx = lastGuess[dotNumber] + ((e.shiftKey) ? -1 : 1) + n_colors;
-  // lastGuess[dotNumber] = colIx % n_colors;
+  // colIx = game['lastguess'][dotNumber] + ((e.shiftKey || e.which == 3 || e.which == 3) ? -1 : 1) + n_colors;
+  // colIx = game['lastguess'][dotNumber] + ((e.shiftKey) ? -1 : 1) + n_colors;
+  // game['lastguess'][dotNumber] = colIx % n_colors;
   $(this).animate({
     backgroundColor: colorsList[colIx % n_colors]
   }, 200, "swing");
@@ -197,14 +205,15 @@ function dotClick2(e) {
 
 function createGame(n_colors, show = false) {
   // genera la combinazione da indovinare
-  configuration = []
+  game['solution'] = []
+  // game['solution'] = game['solution']
   for (i = 0; i < 4; i++)
-    configuration.push(Math.floor(Math.random() * n_colors));
+    game['solution'].push(Math.floor(Math.random() * n_colors));
 
   if (show) // colora/mostra la soluzione (sotto il div nascosto) 
     $('.dot.sol').each(function () {
       dotNumber = parseInt(this.id.split('l')[1]);
-      var clr = colorsList[configuration[dotNumber]];
+      var clr = colorsList[game['solution'][dotNumber]];
       $(this).css('background-color', clr);
     });
 
@@ -220,8 +229,10 @@ function createGame(n_colors, show = false) {
   $('#movesContainer').empty()
   // creates a new one
   newTry();
-  // hides the menu
-  $('#clr_select').hide()
+
+  // disables the menu and selects the first one
+  $('#clr_select').prop('disabled', 'disabled')
+
   // hides the win_message
   if ($('.win_message')) {
     $('.win_message').hide()
@@ -234,5 +245,6 @@ function createGame(n_colors, show = false) {
 
 $(document).ready(function () {
   // $('.try.dot').bind("click", dotClick);
-  $('.try.dot').bind("click", dotClick2);
+  $('.try.dot').bind("click", dotClick);
+  $('#clr_select').prop("selectedIndex", 0);
 });
