bdda790befd6a4eb3371a1088f614f91b7901db5
sbiego <forsesim@anche.no>
2020-04-12 07:13:44 +0200

    added howto and credits

--- js/JasterSind.js
+++ js/JasterSind.js
@@ -239,6 +239,30 @@ function createGame(n_colors, show = false) {
 
 }
 
+/// MODAL PANEL
+buttons = document.querySelectorAll(".modal.btn")
+buttons.forEach(element => {
+  element.addEventListener("click", function () {
+    href = element.getAttribute('href')
+    document.querySelector('#howtos').classList.remove('hidden')
+    document.querySelectorAll('.modal.panel').forEach(element => {
+      element.classList.remove('active')
+      element.classList.add('hidden')
+    })
+    if (href != "#close") {
+      document.querySelector(href).classList.toggle('active')
+      document.querySelector(href).classList.toggle('hidden')
+    } else {
+      // lets the animation run
+      // setTimeout(function () {
+      //   document.querySelector('#howtos').classList.add('hidden')
+      // }, 700)
+    }
+    // console.log(href)
+  })
+})
+
+
 ////////////////////////////////////// MAIN ///////////////////////////
 
 $(document).ready(function () {
